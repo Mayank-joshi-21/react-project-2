6a327b17fd05c659605e2136894b065af099769e
Mayank Joshi <113921344+Mayank-joshi-21@users.noreply.github.com>
2025-07-06 11:36:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import ContactHeader from './components/ContactHeader/ContactHeader'
+import Navigation from './components/Navigation/Navigation'
+import './App.css';
+
+
+const App = () => {
+  return (
+    <div>
+      <Navigation/>
+      <ContactHeader/>
+    </div>
+  )
+}
+
+export default App
+
